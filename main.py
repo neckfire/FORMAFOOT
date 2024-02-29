648513f310142ec8b6ac1195136fee47a3a196ce
ClubInformatiqueIASchoollyon <60139284+neckfire@users.noreply.github.com>
2024-02-29 16:31:16 +0100

    added pdf generation with backgrounds

--- main.py
+++ main.py
@@ -93,9 +93,12 @@ json_data = load_json(file_path)
 formation_instance = Formation(json_data)
 
 class PDF(FPDF):
-    def header(self):
-        self.set_font('Arial', 'B', 12)
-        self.cell(0, 10, 'Formation Details', 0, 1, 'C')
+    def __init__(self):
+        super().__init__()
+        # Set default font
+        self.set_font('Arial', '', 12)
+        # Set background color
+        self.set_fill_color(240, 248, 255)
 
     def chapter_title(self, title):
         self.set_font('Arial', 'B', 12)
@@ -107,18 +110,46 @@ class PDF(FPDF):
         self.multi_cell(0, 10, body)
         self.ln()
 
+    def header(self):
+        # Set font
+        self.set_font('Arial', 'B', 15)
+        # Set fill color for title
+        self.set_text_color(255, 255, 255)
+        # Set background color for title
+        self.set_fill_color(30, 144, 255)
+        # Title
+        self.cell(0, 10, 'Détails de la formation', border=1, ln=1, fill=True, align='C')
+        # Set fill color for rest of the page
+        self.set_fill_color(240, 248, 255)
+        # Line break
+        self.ln(20)
+
+    def add_logo(self, logo_path, x, y, w, h):
+        # Add logo
+        self.image(logo_path, x=x, y=y, w=w, h=h)
+
+    def add_text_block(self, text, x, y, w, h, border=0, align='L',size=12):
+        self.set_xy(x, y)
+        self.set_font('Arial', '', size)
+        self.set_text_color(255, 255, 255)
+        self.multi_cell(w, h, txt=text, border=border, align=align, fill=False)
+        self.ln(h)
+        self.set_fill_color(5, 75, 189)
+        self.add_font("Graebenbach", "", "ressources/fonts/Graebenbach-Medium.otf", uni=True)
+        self.set_font("Graebenbach")
+
 def extract_details(formation_instance):
-    details = f"ID: {formation_instance.id}\n" \
-              f"Référence: {formation_instance.ref}\n" \
-              f"Prix: {formation_instance.prix}\n" \
-              f"Créateur: {formation_instance.createur.nom} {formation_instance.createur.prenom}\n" \
-              f"Catégorie: {formation_instance.categorie}\n" \
-              f"Description: {formation_instance.description}\n"
+    #pdf.add_text_block(f"ID: {formation_instance.id}", x=5, y=5, w=0, h=6)
+    #pdf.add_text_block(f"Prix: {formation_instance.prix}", x=10, y=160, w=0, h=6)
+    #pdf.add_text_block(f"Créateur: {formation_instance.createur.nom} {formation_instance.createur.prenom}", x=10, y=160, w=0, h=6)
+    #pdf.add_text_block(f"Catégorie: {formation_instance.categorie}", x=10, y=160, w=0, h=6)
+    pdf.add_text_block(f"{formation_instance.description}", x=10, y=200, w=0, h=6, align='C', size=23)
+    #pdf.add_text_block(f"Référence: {formation_instance.ref}", x=175, y=257, w=0, h=6)
 
-    return details
 
-def add_chapitre(pdf, chapitre):
+def add_chapitre(pdf, chapitre, chapitreindex):
     pdf.add_page()
+    pdf.add_logo(imageindex[i], x=0, y=0, w=210, h=297)
     pdf.chapter_title(chapitre.chapitre_titre)
     for subchapitre in chapitre.subchapitre:
         pdf.chapter_title(subchapitre.subchapitre_titre)
@@ -126,14 +157,28 @@ def add_chapitre(pdf, chapitre):
             pdf.chapter_body(f"Document ID: {doc.id}\nType: {doc.type}\nDescription: {doc.description}\n")
 
 pdf = PDF()
-
+i=0
+imageindex=("ressources/back/1.png","ressources/back/3.png","ressources/back/2.png","ressources/back/3.png")
 pdf.add_page()
-pdf.chapter_title("Formation Details")
-pdf.chapter_body(extract_details(formation_instance))
+pdf.add_logo(imageindex[i], x=0, y=0, w=210, h=297)
+pdf.add_text_block(formation_instance.titre, x=10, y=150, w=0, h=20, border=1, align='C', size=40)
+extract_details(formation_instance)
 
-for structure in formation_instance.structure:
-    for chapitre in structure.chapitres:
-        add_chapitre(pdf, chapitre)
+#pdf.image('ressources/images/bandeau_white.png', x=10, y=100, w=200, h=60)
+#for structure in formation_instance.structure:
+#    for chapitre in structure.chapitres:
+#        add_chapitre(pdf, chapitre, i)
+
+i+=1
+pdf.add_page()
+pdf.add_logo(imageindex[i], x=0, y=0, w=210, h=297)
+pdf.add_text_block("Finançable: " + str(formation_instance.financeable),x=15,y=40, w=80,h=20, border=0, align='C',size=20)
+pdf.add_text_block("Prix: " + str(formation_instance.prix),x=115,y=80, w=80,h=20, border=0, align='C',size=20)
+pdf.add_text_block("Durée totale: " + str(16),x=7,y=130, w=80,h=20, border=0, align='C',size=20)
+pdf.add_text_block("Pré-requis: \n " + str(formation_instance.prerequis),x=118,y=168, w=76,h=14, border=1, align='C',size=15)
+
+
+
 
 pdf_output_path = 'formation_details.pdf'
 pdf.output(pdf_output_path)
