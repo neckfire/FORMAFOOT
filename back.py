f62eda1df7285fd2be75827b43e7e5e6f47f12ed
Breadoneyeah <162575076+Breadoneyeah@users.noreply.github.com>
2024-03-21 12:33:07 +0100

    edited project

--- back.py
+++ back.py
@@ -45,7 +45,16 @@ class Createur:
         self.nom = createur_data.get('nom')
         self.prenom = createur_data.get('prenom')
 
+
+class Module:
+    def __init__(self, module_data: dict):
+        self.info_sup: dict = module_data.get('info_sup')  # Informations supplémentaires sur le module
+        self.chapitres: List[Chapitre] = [Chapitre(chapitre_data) for chapitre_data in module_data.get('chapitres', [])]  # Liste des chapitres du module
+        self.description: str = module_data.get('description')  # Description du module
+        self.module_titre: str = module_data.get('module_titre')  # Titre du module
+
 class Formation:
+<<<<<<< Updated upstream
     def __init__(self, formation_data):
         self.id = formation_data.get('id')
         self.ref = formation_data.get('ref')
@@ -103,3 +112,58 @@ json_data = load_json(file_path)
 
 formation_instance = Formation(json_data)
 formation_instance.nb_doc = print_document_counts(formation_instance)
+=======
+    def __init__(self, formation_data: dict):
+        self.prix: Optional[float] = formation_data.get('prix')  # Prix de la formation (optionnel)
+        self.titre: str = formation_data.get('titre')  # Titre de la formation
+        self.apports: str = formation_data.get('apports')  # Apports de la formation
+        self.createur: Createur = Createur(formation_data.get('createur', {}))  # Créateur de la formation
+        self.info_sup: dict = formation_data.get('info_sup')  # Informations supplémentaires sur la formation
+        self.categorie: str = formation_data.get('categorie')  # Catégorie de la formation
+        self.prerequis: str = formation_data.get('prerequis')  # Prérequis de la formation
+        self.structure: List[Structure] = [Structure(structure_data) for structure_data in formation_data.get('structure', [])]  # Structure de la formation (liste de structures)
+        self.type_forma: str = formation_data.get('type_forma')  # Type de la formation
+        self.description: str = formation_data.get('description')  # Description de la formation
+        self.financeable: Optional[bool] = formation_data.get('financeable')  # Indicateur de possibilité de financement (optionnel)
+        self.texte_financeable: str = formation_data.get('texte_financeable')  # Texte sur la possibilité de financement
+        self.duree = self.get_duration()  # Durée totale de la formation
+        self.nb_doc: int = self.print_document_counts()
+        self.modules: List[Module] = [Module(module_data) for module_data in formation_data.get('structure', [])]  # Liste des modules de la formation
+
+
+
+    def get_duration(self):
+        total_seconds = 0
+        for structure in self.structure: 
+            for chapitre in structure.chapitres:
+                print("Chapter:", chapitre.chapitre_titre)
+                for subchapitre in chapitre.subchapitre:
+                    print("Subchapter:", subchapitre.subchapitre_titre)
+                    for document in subchapitre.documents:
+                        # Analyse de la durée de la chaîne en secondes
+                        duration_str = document.duree
+                        hours, minutes, seconds = map(int, duration_str.split(':'))
+                        doc_seconds = hours * 3600 + minutes * 60 + seconds
+                        total_seconds += doc_seconds
+                        print("Document Duration:", doc_seconds,"seconds")
+                    print("-----------------------------")
+
+        total_hours, remaining_seconds = divmod(total_seconds, 3600)
+        total_minutes, total_seconds = divmod(remaining_seconds, 60)
+        print("Total duration of the formation:")
+        print(f"{int(total_hours)}H, {int(total_minutes)}, {int(total_seconds)} seconds")
+        total_duration = f"{int(total_hours)} H {int(total_minutes)}"
+        return total_duration
+    
+    def print_document_counts(self):
+        nombres_doc = 0
+        for structure in self.structure:
+            for chapitre in structure.chapitres:
+                print("Chapter:", chapitre.chapitre_titre)
+                for subchapitre in chapitre.subchapitre:
+                    print("Subchapter:", subchapitre.subchapitre_titre)
+                    print("Number of documents:", len(subchapitre.documents))
+                    nombres_doc += len(subchapitre.documents)
+                    print("-----------------------------")
+        return nombres_doc
+>>>>>>> Stashed changes
--- front.py
+++ front.py
@@ -2,10 +2,19 @@ from fpdf import FPDF
 from back import formation_instance
 from math import ceil
 
+<<<<<<< Updated upstream
 
 class PDF(FPDF):
     def __init__(self):
         super().__init__()
+=======
+class PDF(FPDF):
+    def __init__(self, formation: Formation):
+        super().__init__()
+        self.formation_instance = formation
+        self.imageindex=("ressources/back/1.png","ressources/back/3.png","ressources/back/2.png","ressources/back/3.png","ressources/images/sticker_round.png")
+        self.set_y=40
+>>>>>>> Stashed changes
         # Set default font
         self.set_font('Arial', '', 12)
         # Set background color
@@ -39,7 +48,6 @@ class PDF(FPDF):
         self.image(logo_path, x=x, y=y, w=w, h=h)
 
     def add_text_block(self, text, x, y, w, h, border=0, align='L',size=12):
-        self.set_xy(x, y)
         self.add_font("mediasans","", "ressources/fonts/mediasans-regular.otf", uni=True)
         self.set_font('mediasans', '', size)
         self.set_text_color(255, 255, 255)
@@ -49,6 +57,7 @@ class PDF(FPDF):
         #self.add_font("Graebenbach", "", "ressources/fonts/Graebenbach-Medium.otf", uni=True)
         #self.set_font("Graebenbach")
 
+<<<<<<< Updated upstream
 def extract_details(formation_instance):
     #pdf.add_text_block(f"ID: {formation_instance.id}", x=5, y=5, w=0, h=6)
     #pdf.add_text_block(f"Prix: {formation_instance.prix}", x=10, y=160, w=0, h=6)
@@ -56,6 +65,42 @@ def extract_details(formation_instance):
     #pdf.add_text_block(f"Catégorie: {formation_instance.categorie}", x=10, y=160, w=0, h=6)
     pdf.add_text_block(f"{formation_instance.description}", x=10, y=200, w=0, h=6, align='C', size=23)
     #pdf.add_text_block(f"Référence: {formation_instance.ref}", x=175, y=257, w=0, h=6)
+=======
+    def create_pdf(self):
+        i=0
+        self.add_page()
+        self.add_logo(self.imageindex[i], x=0, y=0, w=210, h=297)
+        self.add_text_block(self.formation_instance.titre, x=10, y=150, w=0, h=20, border=1, align='C', size=40)
+        self.extract_details()
+        i+=1
+        self.add_page()
+        self.add_logo(self.imageindex[i], x=0, y=0, w=210, h=297)
+        self.add_text_block("Finançable: " + str(self.formation_instance.financeable),x=15,y=40, w=80,h=20, border=0, align='C',size=20)
+        self.add_text_block("Prix: " + str(self.formation_instance.prix),x=115,y=80, w=80,h=20, border=0, align='C',size=20)
+        self.add_text_block("Durée totale:\n" + str(self.formation_instance.duree),x=7,y=120, w=80,h=20, border=0, align='C',size=20)
+        self.set_fill_color(4, 74, 189)
+        #Rect(float x, float y, float w, float h [, string style])
+        self.rect(x=118,y=168, w=76,h=ceil((self.get_string_width(str(self.formation_instance.prerequis))/76))*15,style='F')
+        self.add_text_block("Pré-requis: \n " + str(self.formation_instance.prerequis),x=118,y=168, w=76,h=14, border=0, align='C',size=15)
+        self.add_text_block("Nombre de documents: \n " + str(self.formation_instance.nb_doc),x=7,y=225, w=80,h=10, border=0, align='C',size=15)
+        self.add_page_nb(2)
+        i+=1
+        self.add_page()
+        self.add_logo(self.imageindex[i], x=0, y=0, w=210, h=297)
+        self.listeverything()
+
+    def extract_details(self):
+        #pdf.add_text_block(f"ID: {formation_instance.id}", x=5, y=5, w=0, h=6)
+        #pdf.add_text_block(f"Prix: {formation_instance.prix}", x=10, y=160, w=0, h=6)
+        #pdf.add_text_block(f"Créateur: {formation_instance.createur.nom} {formation_instance.createur.prenom}", x=10, y=160, w=0, h=6)
+        #pdf.add_text_block(f"Catégorie: {formation_instance.categorie}", x=10, y=160, w=0, h=6)
+        self.add_text_block(f"{self.formation_instance.description}", x=10, y=200, w=0, h=6, align='C', size=23)
+        #pdf.add_text_block(f"Référence: {formation_instance.ref}", x=175, y=257, w=0, h=6)
+
+    def add_page_nb(self, nbpage: int):
+        self.add_logo(self.imageindex[4], x=85,y=262,w=30,h=30)
+        self.add_text_block(str(nbpage), x=90,y=275,w=20,h=2, border=0, align='C',size=7)
+>>>>>>> Stashed changes
 
 
 def add_chapitre(pdf, chapitre, chapitreindex):
@@ -67,6 +112,7 @@ def add_chapitre(pdf, chapitre, chapitreindex):
         for doc in subchapitre.documents:
             pdf.chapter_body(f"Document ID: {doc.id}\nType: {doc.type}\nDescription: {doc.description}\n")
 
+<<<<<<< Updated upstream
 
 pdf = PDF()
 i=0
@@ -92,3 +138,52 @@ pdf.set_fill_color(4, 74, 189)
 pdf.rect(x=118,y=168, w=76,h=ceil((pdf.get_string_width(str(formation_instance.prerequis))/76))*15,style='F')
 pdf.add_text_block("Pré-requis: \n " + str(formation_instance.prerequis),x=118,y=168, w=76,h=14, border=0, align='C',size=15)
 pdf.add_text_block("Nombre de documents: \n " + str(formation_instance.nb_doc),x=7,y=225, w=80,h=10, border=0, align='C',size=15)
+=======
+    def listeverything(self):
+        print("test")
+        if self.set_y>250:
+           self.set_y=40
+           self.add_page()
+        for module in self.formation_instance.modules:
+            print("test2")
+            self.add_text_block("Module: \n " + str(module.module_titre), x = 10, y = self.set_y, w=80,h=30, border=0, align='C',size=15)
+            self.set_y += 15
+            if self.set_y>250:
+               self.set_y = 40
+               self.add_page()
+            for chapitre in module.chapitres:
+               self.add_text_block("Chapitre: \n " + str(chapitre.chapitre_titre), x = 40, y = self.set_y, w=85,h=20, border=0, align='C',size=15)
+               self.set_y += 15
+               if self.set_y>250:
+                   self.set_y=40
+                   self.add_page()
+               for sub in chapitre.subchapitre:
+                   self.add_text_block("Sous-chapitres: \n " + str(sub.subchapitre_titre), x = 70, y = self.set_y, w=90,h=14, border=0, align='C',size=15)
+                   self.set_y += 15
+                   if self.set_y>250:
+                       self.set_y=40
+                       self.add_page()
+                   for document in sub:
+                       self.add_text_block("Documents: \n " + str(len(sub.documents)), document, x = 100, y = self.set_y, w=100,h=12, border=0, align='C',size=15)
+                       self.set_y += 15
+                       if self.set_y>250:
+                           self.set_y=40
+                           self.add_page()
+                
+    # def listeverything(self, formation_instance):
+    #     self.add_page()
+    #     y = 20  # Starting y position
+    #     for module in formation_instance.modules:
+    #         self.set_font('Arial', 'B', 12)
+    #         self.cell(0, 10, f'Module: {module.module_titre}', 0, 1, 'L')
+    #         for chapitre in module.chapitres:
+    #             self.set_font('Arial', 'B', 10)
+    #             self.cell(0, 10, f'Chapitre: {chapitre.chapitre_titre}', 0, 1, 'L')
+    #             for subchapitre in chapitre.subchapitre:
+    #                 self.set_font('Arial', 'I', 10)
+    #                 self.cell(0, 10, f'Sous-chapitre: {subchapitre.subchapitre_titre}', 0, 1, 'L')
+    #                 for document in subchapitre.documents:
+    #                     self.set_font('Arial', '', 10)
+    #                     self.cell(0, 10, f'Document: {document.file}', 0, 1, 'L')
+    #     self.output('formation_details.pdf')
+>>>>>>> Stashed changes
